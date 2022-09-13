bb6f15e9082e2c06381176e03914114352e7d333
Vladimir <madfrog@e1.ru>
2022-09-13 17:52:26 +0300

    Created Task 2

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("1111111Hello, World!");
+﻿try
+{
+    Console.WriteLine("Введите первое число ");
+    int x=Convert.ToInt32 (Console.ReadLine());
+    Console.WriteLine("Введите второе число ");
+    int y=Convert.ToInt32 (Console.ReadLine());
+    if (x>y)
+        {
+        Console.WriteLine("Большее число "+x);
+        }
+    else Console.WriteLine("Большее число "+y);
+}
+catch
+{
+Console.WriteLine("Необходимо ввести число цифрами");
+}
